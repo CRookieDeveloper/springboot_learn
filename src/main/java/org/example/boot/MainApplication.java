2d57701af3db2e90e35411f7f1aa39c1ad196123
RookieFpp <cfpp1995@gmail.com>
2022-08-21 20:06:01 +0800

    #commit 创建springboot程序，完成pom文件配置和主程序撰写

---
+++ src/main/java/org/example/boot/MainApplication.java
@@ -0,0 +1,21 @@
+package org.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: springboot-first
+ * @BelongsPackage: org.example.boot
+ * @ClassName MainApplication
+ * @Author: RookieFu
+ * @CreateTime: 2022-08-21  20:02
+ * @UpdateTime: 2022-08-21  20:02
+ * @Description: TODO
+ * @Version:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
